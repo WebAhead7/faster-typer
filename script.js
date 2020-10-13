edb5e93d44443fcfb7830c7510958894fb6546cd
alaabashiyi <alaa.bashiy@gmail.com>
2020-10-13 17:48:13 +0300

    added js file and some logic with the guys Related #2

--- script.js
+++ script.js
@@ -5,30 +5,50 @@ const currentScore = document.querySelector('#score');
 const highiestScore = document.querySelector('#high-score');
 const level = document.querySelector('#level-list');
 
+input.focus();
 const words = ['can','type', 'faster','biolog','biology','designer', 'development', 'javascript','webahead7'];
 
+let lvl1 = 7;
+let lvl2 = 5;
+let lvl3 = 2;
+
+
 let score = 0;
-let time = 7;
+let time = 10;
 let randomText;
 
-let timer = 0;
+let flag;
+// let userInput = input.value;
+
+
+let timer = setInterval(updateTimer, 1000);
+
 
 function randomWord(){
     randomText = words[Math.floor(Math.random() * words.length )];
+    currentWord.innerText = randomText;
 }
+
+(function startGame(){
+    randomWord();      
+      }
+  )();
 
 
 function showWord(){
     currentWord.innerText = randomText;
 }
 
-function checkWord(curr, typed) {
-    curr === typed ? true : false;
-
+function checkWords() {
+    if(randomText === input.value){
+         return true;
+    }else{
+        return false;
+    }
 };
 
 function updateScore() {
-
+    score++;
 };
 
 function gameOver() {
@@ -39,16 +59,32 @@ function restartGame(){
 
 }
 
+
+
 function updateTimer(){
     time--;
     // currentTimer.innerText = `${time}s`;
     if(time === 0) {
-        clearInterval(timer);
+        clearInterval(timer);  
+        gameOver();
     }
+    console.log(time);
 };
 
 input.addEventListener('keyup',(e)=>{
-    if(e.target){
-        timer = setInterval(updateTimer, 1000);
+    if(input.value.length == randomText.length){
+       flag = checkWords();
+       console.log(flag);
+       if(!flag){
+           randomWord();
+           input.value = '';
+       } else {
+           time += lvl2;
+           input.value = '';
+           updateScore();
+           randomWord();
+       }
     }
+
+    // console.log(randomText.length, randomText);
 })
